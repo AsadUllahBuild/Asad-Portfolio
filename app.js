9b7df565cff102c371f73631e1e09d9ef113804d
AsadUllahBuild <asadbuild97@gmail.com>
2023-12-14 01:27:23 +0500

    my portfolio

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    this.classList.toggle('change');
+    document.querySelector('.nav-links').classList.toggle('show');
+});
+
