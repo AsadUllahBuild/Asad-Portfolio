0c2650caceec5a569d49a347e02054651852a159
AsadUllahBuild <asadbuild97@gmail.com>
2023-12-25 01:52:47 +0500

    portfolio-complete

--- app.js
+++ app.js
@@ -9,7 +9,27 @@ var typed = new Typed(".text", {
 
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
     this.classList.toggle('change');
-    document.querySelector('.nav-links').classList.toggle('show');
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+
+    const homeSection = document.querySelector('#home');
+
+    // If nav-links is open
+    if (navLinks.classList.contains('show')) {
+        // Calculate the height of the nav-links element
+        const navLinksHeight = navLinks.getBoundingClientRect().height;
+
+        // Get the top position of the home section
+        const homeSectionTop = homeSection.offsetTop;
+
+        // Scroll to the top of the home section adjusted by the nav-links height
+        window.scrollTo({ top: homeSectionTop - navLinksHeight, behavior: 'smooth' });
+    } else {
+        // Scroll back to the top when nav-links is closed
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 });
+
+
 
 
